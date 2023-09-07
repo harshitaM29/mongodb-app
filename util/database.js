17fc9c077815e9d37d013c867e8365ca4fe49473
harshitaM29 <harshita.mundhe29@gmail.com>
2023-09-07 12:23:21 +0530

    CRUD operation using mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,29 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://harshitaM:va5ptYpFmS8lTMpx@cluster0.uazqkfu.mongodb.net/shop?retryWrites=true&w=majority')
+.then(client => {
+  console.log('connected');
+  _db = client.db()
+  callback();
+})
+.catch(err => {
+  console.log(err);
 });
 
-module.exports = sequelize;
+};
+
+const getDb = () => {
+  if(_db) {
+    return _db;
+  }
+  throw 'No Database found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
