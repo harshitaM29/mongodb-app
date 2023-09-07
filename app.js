26adbd8291cde3274da7204a00a79585ae62242d
harshitaM29 <harshita.mundhe29@gmail.com>
2023-09-07 17:57:17 +0530

    user model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
-
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,13 +18,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findByUserId('64f9bfb15687e8181245f555')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+  // next();
 });
 
 app.use('/admin', adminRoutes);
