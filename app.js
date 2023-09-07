8e62bd1ae0282182ad74df42e675faa942db777f
harshitaM29 <harshita.mundhe29@gmail.com>
2023-09-07 18:23:16 +0530

    cart functionality

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findByUserId('64f9bfb15687e8181245f555')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const getDb = require('../util/database').getDb;
 const mongodb = require('mongodb');
 
 class User {
-  constructor(id,name,email) {
+  constructor(id,name,email, cart) {
     this.name = name;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
@@ -18,6 +20,17 @@ class User {
     })
   }
 
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+   
+    const updatedCart = {items: [{productId: new mongodb.ObjectId(product._id), quantity:1}]};
+    const db = getDb();
+   return db.collection('users').updateOne({_id: new mongodb.ObjectId(this._id)}, 
+    { $set: {cart: updatedCart}}
+    )
+  }
   static findByUserId(userId) {
     const db = getDb();
     return db.collection('users').findOne({_id: new mongodb.ObjectId(userId)})
